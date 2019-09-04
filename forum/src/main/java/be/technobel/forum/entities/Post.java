b38625bda13902cffc7d05c722d8fb5002bd1390
inuth <49624288+inuth@users.noreply.github.com>
2019-09-04 11:39:11 +0200

    Update Post.java

--- forum/src/main/java/be/technobel/forum/entities/Post.java
+++ forum/src/main/java/be/technobel/forum/entities/Post.java
@@ -4,13 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Column;
 
 @Entity
 public class Post {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	
+	@Column
 	private String name;
 	
 	public Integer getId() {
