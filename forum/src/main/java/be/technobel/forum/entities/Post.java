e103fbdc51a0428670964401c46b4b22088eb402
GOger <GOger@DESKTOP-3830PTT>
2019-09-04 11:43:18 +0200

    modification entité post

--- forum/src/main/java/be/technobel/forum/entities/Post.java
+++ forum/src/main/java/be/technobel/forum/entities/Post.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Post {
@@ -11,20 +12,29 @@ public class Post {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
-	private String name;
+	private String content;
 	
+	@ManyToOne
+	private User author;
 	public Integer getId() {
 		return id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
+	public String getContent() {
+		return content;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setContent(String name) {
+		this.content = name;
 	}
+	public User getAuthor() {
+		return author;
+	}
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+	
 	
 	
 }
